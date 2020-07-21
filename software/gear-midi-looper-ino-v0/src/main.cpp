5bc3709bd383256b3882f7793faa1ce756805abf
Rafael Khan <raf.khan.f@gmail.com>
2020-07-21 00:52:31 -0400

    Trying some stuff...

--- software/gear-midi-looper-ino-v0/src/main.cpp
+++ software/gear-midi-looper-ino-v0/src/main.cpp
@@ -12,34 +12,127 @@
 
 MIDI_CREATE_INSTANCE(HardwareSerial, Serial1, MIDI);
 
+const int _MIDI_CLOCK_BYTE = 0xF8;
+const int _MIDI_START_BYTE = 0xFA;
+const int _MIDI_STOP_BYTE = 0xFC;
+const int _MAX_CLOCK_COUNTER_SIZE = 64;
+const int _MAX_EVENT_BUFFER_SIZE = 128;
+
+typedef struct midi_event_t {
+  int lastClock;
+  uint32_t timeSinceLastClock;
+  byte midiEventType;
+  int channel;
+  int note;
+  int velocity;
+};
+
+// playhead / clock counting
+volatile int clockCounter = 0;
+uint32_t lastClockTime = 0;
+midi_event_t events[_MAX_CLOCK_COUNTER_SIZE][_MAX_EVENT_BUFFER_SIZE];
+int lastRecordedEventCounter[_MAX_CLOCK_COUNTER_SIZE];
+
+unsigned long t = 0;
+
+void onMidiClock() {
+  lastClockTime = micros();
+
+  if(clockCounter < _MAX_CLOCK_COUNTER_SIZE) {
+    clockCounter++;
+    return;
+  }
+
+  Serial.println(String("RESETTING CLOCK COUNTER"));
+  clockCounter = 0;
+  for(int i = 0; i < _MAX_CLOCK_COUNTER_SIZE; i++) {
+    lastRecordedEventCounter[i] = 0;
+  }
+}
+
+void onMidiStart() {
+  Serial.println(String("MIDI Start"));
+}
+
+void onMidiStop() {
+  Serial.println(String("MIDI Stop"));
+}
+
+void onMidiNoteOn(int channel, int note, int velocity) {
+  Serial.println(String("Note On:  ch=") + channel + ", note=" + note + ", velocity=" + velocity);
+  midi_event_t e;
+  e.lastClock = clockCounter;
+  e.timeSinceLastClock = micros() - lastClockTime;
+  e.note = note;
+  e.channel = channel;
+  e.velocity = velocity;
+  e.midiEventType = midi::NoteOn;
+  events[clockCounter][lastRecordedEventCounter[clockCounter]] = e;
+
+  if(lastRecordedEventCounter[clockCounter] < _MAX_EVENT_BUFFER_SIZE) {
+    lastRecordedEventCounter[clockCounter]++;
+  } else {
+    lastRecordedEventCounter[clockCounter] = 0;
+  }
+}
+
+void onMidiNoteOff(int channel, int note, int velocity) {
+  Serial.println(String("Note Off: ch=") + channel + ", note=" + note + ", velocity=" + velocity);
+  midi_event_t e;
+  e.lastClock = clockCounter;
+  e.timeSinceLastClock = micros() - lastClockTime;
+  e.note = note;
+  e.channel = channel;
+  e.velocity = velocity;
+  e.midiEventType = midi::NoteOff;
+  events[clockCounter][lastRecordedEventCounter[clockCounter]] = e;
+
+  if(lastRecordedEventCounter[clockCounter] < _MAX_EVENT_BUFFER_SIZE) {
+    lastRecordedEventCounter[clockCounter]++;
+  } else {
+    lastRecordedEventCounter[clockCounter] = 0;
+  }
+}
+
 void setup() {
   MIDI.begin(MIDI_CHANNEL_OMNI);
   Serial.begin(57600);
   Serial.println("MIDI Input Test");
+
+  for(int i = 0; i < _MAX_CLOCK_COUNTER_SIZE; i++) {
+    lastRecordedEventCounter[i] = 0;
+  }
 }
 
-unsigned long t=0;
-
+int type, note, velocity, channel, d1, d2;
 void loop() {
-  int type, note, velocity, channel, d1, d2;
   if (MIDI.read()) {                    // Is there a MIDI message incoming ?
     byte type = MIDI.getType();
     switch (type) {
       case midi::NoteOn:
+        channel = MIDI.getChannel();
         note = MIDI.getData1();
         velocity = MIDI.getData2();
-        channel = MIDI.getChannel();
         if (velocity > 0) {
-          Serial.println(String("Note On:  ch=") + channel + ", note=" + note + ", velocity=" + velocity);
+          onMidiNoteOn(channel, note, velocity);
         } else {
-          Serial.println(String("Note Off: ch=") + channel + ", note=" + note);
+          onMidiNoteOff(channel, note, velocity);
         }
         break;
       case midi::NoteOff:
         note = MIDI.getData1();
         velocity = MIDI.getData2();
         channel = MIDI.getChannel();
-        Serial.println(String("Note Off: ch=") + channel + ", note=" + note + ", velocity=" + velocity);
+        onMidiNoteOff(channel, note, velocity);
+        break;
+      case _MIDI_CLOCK_BYTE:
+        onMidiClock();
+        break;
+      case _MIDI_START_BYTE:
+        onMidiStart();
+        break;
+      case _MIDI_STOP_BYTE:
+        onMidiStop();
         break;
       default:
         d1 = MIDI.getData1();
